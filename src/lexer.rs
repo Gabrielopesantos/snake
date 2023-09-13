c6734131366077cf848dc1cd400b4bc10653691f
Gabriel Santos <gabrielopesantos97@gmail.com>
2023-09-13 23:17:02 +0100

    test for lexer and remove start and end of token

--- src/lexer.rs
+++ src/lexer.rs
@@ -6,9 +6,11 @@ pub enum Token {
     Assign,
     Plus,
     Minus,
-    Multiply,
-    Divide,
+    Asterisk,
+    Slash,
     Bang,
+    Lt,
+    Gt,
     Eq,
     NotEq,
     Comma,
@@ -30,21 +32,11 @@ pub enum Token {
     Return,
 }
 
-#[derive(Debug, PartialEq)]
-pub struct TokenWithLoc {
-    pub token: Token,
-    pub start: usize,
-    pub end: usize,
-}
-
-pub fn lex(input: &str) -> Result<Vec<TokenWithLoc>, &'static str> {
+pub fn lex(input: &str) -> Result<Vec<Token>, &'static str> {
     let mut tokens = Vec::new();
     let mut chars = input.char_indices().peekable();
-    let mut current_start = 0;
 
-    while let Some((idx, c)) = chars.next() {
-        let mut current_end = idx + 1;
-
+    while let Some((_, c)) = chars.next() {
         match c {
             '0'..='9' => {
                 let mut value = String::from(c);
@@ -53,7 +45,6 @@ pub fn lex(input: &str) -> Result<Vec<TokenWithLoc>, &'static str> {
                     match next {
                         '0'..='9' | '.' => {
                             value.push(next);
-                            current_end += 1;
                             chars.next();
                             if next == '.' {
                                 // Check if `is_float` has been set before, if so,
@@ -78,113 +69,30 @@ pub fn lex(input: &str) -> Result<Vec<TokenWithLoc>, &'static str> {
                     token = Token::Integer(parsed_value);
                 }
 
-                tokens.push(TokenWithLoc {
-                    token,
-                    start: current_start,
-                    end: current_end,
-                });
+                tokens.push(token);
             }
-            '+' => {
-                tokens.push(TokenWithLoc {
-                    token: Token::Plus,
-                    start: current_start,
-                    end: current_end,
-                });
-            }
-            '-' => {
-                tokens.push(TokenWithLoc {
-                    token: Token::Minus,
-                    start: current_start,
-                    end: current_end,
-                });
-            }
-            '*' => {
-                tokens.push(TokenWithLoc {
-                    token: Token::Multiply,
-                    start: current_start,
-                    end: current_end,
-                });
-            }
-            '/' => {
-                tokens.push(TokenWithLoc {
-                    token: Token::Divide,
-                    start: current_start,
-                    end: current_end,
-                });
-            }
-            ',' => tokens.push(TokenWithLoc {
-                token: Token::Comma,
-                start: current_start,
-                end: current_end,
-            }),
-            ':' => tokens.push(TokenWithLoc {
-                token: Token::Colon,
-                start: current_start,
-                end: current_end,
-            }),
-            ';' => tokens.push(TokenWithLoc {
-                token: Token::Semicolon,
-                start: current_start,
-                end: current_end,
-            }),
-            '(' => {
-                tokens.push(TokenWithLoc {
-                    token: Token::LParen,
-                    start: current_start,
-                    end: current_end,
-                });
-            }
-            ')' => {
-                tokens.push(TokenWithLoc {
-                    token: Token::RParen,
-                    start: current_start,
-                    end: current_end,
-                });
-            }
-            '[' => {
-                tokens.push(TokenWithLoc {
-                    token: Token::LBracket,
-                    start: current_start,
-                    end: current_end,
-                });
-            }
-            ']' => {
-                tokens.push(TokenWithLoc {
-                    token: Token::RBracket,
-                    start: current_start,
-                    end: current_end,
-                });
-            }
-            '{' => {
-                tokens.push(TokenWithLoc {
-                    token: Token::LBrace,
-                    start: current_start,
-                    end: current_end,
-                });
-            }
-            '}' => {
-                tokens.push(TokenWithLoc {
-                    token: Token::RBrace,
-                    start: current_start,
-                    end: current_end,
-                });
-            }
+            '+' => tokens.push(Token::Plus),
+            '-' => tokens.push(Token::Minus),
+            '*' => tokens.push(Token::Asterisk),
+            '/' => tokens.push(Token::Slash),
+            ',' => tokens.push(Token::Comma),
+            ':' => tokens.push(Token::Colon),
+            ';' => tokens.push(Token::Semicolon),
+            '(' => tokens.push(Token::LParen),
+            ')' => tokens.push(Token::RParen),
+            '[' => tokens.push(Token::LBracket),
+            ']' => tokens.push(Token::RBracket),
+            '{' => tokens.push(Token::LBrace),
+            '}' => tokens.push(Token::RBrace),
+            '<' => tokens.push(Token::Lt),
+            '>' => tokens.push(Token::Gt),
             '=' => {
                 if let Some(&(_, next)) = chars.peek() {
                     if next == '=' {
-                        current_end += 1;
-                        tokens.push(TokenWithLoc {
-                            token: Token::Eq,
-                            start: current_start,
-                            end: current_end,
-                        });
+                        tokens.push(Token::Eq);
                         chars.next();
                     } else {
-                        tokens.push(TokenWithLoc {
-                            token: Token::Assign,
-                            start: current_start,
-                            end: current_end,
-                        })
+                        tokens.push(Token::Assign)
                     }
                 } else {
                     return Err("Invalid character in input");
@@ -193,19 +101,10 @@ pub fn lex(input: &str) -> Result<Vec<TokenWithLoc>, &'static str> {
             '!' => {
                 if let Some(&(_, next)) = chars.peek() {
                     if next == '=' {
-                        current_end += 1;
-                        tokens.push(TokenWithLoc {
-                            token: Token::NotEq,
-                            start: current_start,
-                            end: current_end,
-                        });
+                        tokens.push(Token::NotEq);
                         chars.next();
                     } else {
-                        tokens.push(TokenWithLoc {
-                            token: Token::Bang,
-                            start: current_start,
-                            end: current_end,
-                        })
+                        tokens.push(Token::Bang)
                     }
                 } else {
                     return Err("Invalid character in input");
@@ -213,18 +112,10 @@ pub fn lex(input: &str) -> Result<Vec<TokenWithLoc>, &'static str> {
             }
             '"' => {
                 let mut string_literal = String::new();
-                current_start = idx; // Update start position to include the opening double quote
                 while let Some(&(_, next)) = chars.peek() {
-                    // Update end position to include the closing double quote
-                    current_end += 1;
-                    // Consume whatever char comes next, string char or closing double quote
                     chars.next();
                     if next == '"' {
-                        tokens.push(TokenWithLoc {
-                            token: Token::String(string_literal),
-                            start: current_start,
-                            end: current_end,
-                        });
+                        tokens.push(Token::String(string_literal));
                         break;
                     } else {
                         string_literal.push(next)
@@ -233,37 +124,27 @@ pub fn lex(input: &str) -> Result<Vec<TokenWithLoc>, &'static str> {
             }
             ' ' | '\t' | '\n' | '\r' => {
                 // Update start position to the next character
-                current_start = idx + 1;
+                ()
             }
             // Identifiers or keywords
             _ => {
                 if is_valid_identifier_char(c) {
                     // TODO: Same as parsing the string
                     let mut string_literal = String::from(c);
-                    current_start -= 1; // Update start position to include the first character
                     while let Some(&(_, next)) = chars.peek() {
                         if next == ' ' || !is_valid_identifier_char(next) {
                             match token_keyword_literal_to_enum(&string_literal) {
                                 Some(token) => {
-                                    tokens.push(TokenWithLoc {
-                                        token,
-                                        start: current_start,
-                                        end: current_end,
-                                    });
+                                    tokens.push(token);
                                 }
                                 None => {
-                                    tokens.push(TokenWithLoc {
-                                        token: Token::Ident(string_literal),
-                                        start: current_start,
-                                        end: current_end,
-                                    });
+                                    tokens.push(Token::Ident(string_literal));
                                 }
                             }
                             break;
                         } else {
                             string_literal.push(next)
                         }
-                        current_end += 1;
                         chars.next();
                     }
                 } else {
@@ -291,4 +172,139 @@ fn token_keyword_literal_to_enum(token_literal: &str) -> Option<Token> {
 
 fn is_valid_identifier_char(ch: char) -> bool {
     ch == '_' || ch.is_alphabetic()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{lex, Token};
+
+    #[test]
+    fn test_lexer() {
+        let input = "let five = 5;
+let ten = 10;
+
+fn add(x, y) {
+    return x + y;
+}
+
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
+
+
+if (5 < 10) {
+    return true;
+} else {
+    return false;
+}
+
+
+10 == 10.2;
+10 != 9.6;
+\"foobar\"
+\"foo bar\"
+[1, 2];
+{\"foo\": \"bar\"}
+";
+
+        let expected_tokens: Vec<Token> = vec![
+            Token::Let,
+            Token::Ident("five".to_string()),
+            Token::Assign,
+            Token::Integer(5),
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident("ten".to_string()),
+            Token::Assign,
+            Token::Integer(10),
+            Token::Semicolon,
+            Token::Fn,
+            Token::Ident("add".to_string()),
+            Token::LParen,
+            Token::Ident("x".to_string()),
+            Token::Comma,
+            Token::Ident("y".to_string()),
+            Token::RParen,
+            Token::LBrace,
+            Token::Return,
+            Token::Ident("x".to_string()),
+            Token::Plus,
+            Token::Ident("y".to_string()),
+            Token::Semicolon,
+            Token::RBrace,
+            Token::Let,
+            Token::Ident("result".to_string()),
+            Token::Assign,
+            Token::Ident("add".to_string()),
+            Token::LParen,
+            Token::Ident("five".to_string()),
+            Token::Comma,
+            Token::Ident("ten".to_string()),
+            Token::RParen,
+            Token::Semicolon,
+            Token::Bang,
+            Token::Minus,
+            Token::Slash,
+            Token::Asterisk,
+            Token::Integer(5),
+            Token::Semicolon,
+            Token::Integer(5),
+            Token::Lt,
+            Token::Integer(10),
+            Token::Gt,
+            Token::Integer(5),
+            Token::Semicolon,
+            Token::If,
+            Token::LParen,
+            Token::Integer(5),
+            Token::Lt,
+            Token::Integer(10),
+            Token::RParen,
+            Token::LBrace,
+            Token::Return,
+            Token::True,
+            Token::Semicolon,
+            Token::RBrace,
+            Token::Else,
+            Token::LBrace,
+            Token::Return,
+            Token::False,
+            Token::Semicolon,
+            Token::RBrace,
+            Token::Integer(10),
+            Token::Eq,
+            Token::Float(10.2),
+            Token::Semicolon,
+            Token::Integer(10),
+            Token::NotEq,
+            Token::Float(9.6),
+            Token::Semicolon,
+            Token::String("foobar".to_string()),
+            Token::String("foo bar".to_string()),
+            Token::LBracket,
+            Token::Integer(1),
+            Token::Comma,
+            Token::Integer(2),
+            Token::RBracket,
+            Token::Semicolon,
+            Token::LBrace,
+            Token::String("foo".to_string()),
+            Token::Colon,
+            Token::String("bar".to_string()),
+            Token::RBrace,
+            Token::Semicolon,
+        ];
+
+        match lex(input) {
+            Ok(tokens) => {
+                for (i, token) in tokens.iter().enumerate() {
+                    println!("Token: {:?}", token);
+                    assert_eq!(expected_tokens[i], *token);
+                }
+            }
+            Err(err) => {
+                panic!("Lexer error: {}", err);
+            }
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,3 @@
 mod lexer;
 
-use lexer::lex;
-
-fn main() {
-    let input = "
-print(\"Hello world\");
-print(\"\");
-let x = 5* 3;
-fn add_two(number: int) int {
-    return number + 2;
-}
-print(add_two(x));
-";
-    match lex(input) {
-        Ok(tokens) => {
-            for token in &tokens {
-                println!(
-                    "Token: {:?}, Start: {}, End: {}",
-                    token.token, token.start, token.end
-                );
-            }
-        }
-        Err(err) => {
-            println!("Lexer error: {}", err);
-        }
-    }
-}
+fn main() {}
